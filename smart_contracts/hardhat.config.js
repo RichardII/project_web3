a26c31d21f462d670e0afe461311a4e596da2c3d
Your Name <you@example.com>
2023-04-13 17:51:43 +0100

    thrid commit

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+// https://eth-sepolia.g.alchemy.com/v2/lKZ03UXvEyr2jBQCCrcLsF1jbFwxq2u5
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,7 +10,13 @@ module.exports = {
   networks: {
     localhost: {
       url: 'http://127.0.0.1:8545',
-      accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'],
+      accounts: ['ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'],
     },
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/lKZ03UXvEyr2jBQCCrcLsF1jbFwxq2u5',
+      accounts: ['b7f9a25e7099cbc69ab5822f398700ceedf94768f59f7cdadd85c0b7ab572516'],
+    },
+
+    
   },
 };
